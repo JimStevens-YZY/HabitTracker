7df5b3922b4d17b2a41ad04637d39e166bd7d249
JimStevens-YZY <1527296253@qq.com>
2024-02-18 21:28:40 +0000

    Add Group Page

--- lib/pages/tabs/group.dart
+++ lib/pages/tabs/group.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../res/listData.dart';
 
 class GroupPage extends StatelessWidget {
   const GroupPage({Key? key}) : super(key: key);
@@ -36,35 +37,27 @@ class _GroupMainState extends State<GroupMain> with TickerProviderStateMixin {
     });
   }
 
-
-/*
   List<Widget> _getListData() {
-    var tempList=listData.map((value){
+    var tempList = listData.map((value) {
       return Container(
         decoration: BoxDecoration(
             border: Border.all(
-                color:const Color.fromRGBO(233, 233,233, 0.9),
-                width: 1
-            ) ),
-        child:Column(
+                color: const Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
+        child: Column(
           children: <Widget>[
             Image.network(value['imageUrl']),
             const SizedBox(height: 12),
             Text(
               value['title'],
               textAlign: TextAlign.center,
-              style:   const TextStyle(
-                  fontSize: 20
-              ),
-            ) ],
-        ), );
+              style: const TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      );
     });
-*/
-
-    // ('xxx','xxx')
     return tempList.toList();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -181,7 +174,95 @@ class _GroupMainState extends State<GroupMain> with TickerProviderStateMixin {
                         ],
                       ),
                     ),
+                    const SizedBox(
+                      width: double.infinity,
+                      height: 40,
+                    ),
+                    const Text("Browse by categories",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    const SizedBox(
+                      width: double.infinity,
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Wrap(
+                        alignment: WrapAlignment.spaceBetween,
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    "https://seniorassistant.oss-cn-hangzhou.aliyuncs.com/zust-lcy-path/20230406/2023040622447.jpg",
+                                    height: 140,
+                                    width: 195,
+                                    fit: BoxFit.cover),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(5),
+                                child: Text("Medway",
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w600,
+                                        color:
+                                            Color.fromRGBO(53, 52, 77, 1.0))),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    "https://seniorassistant.oss-cn-hangzhou.aliyuncs.com/zust-lcy-path/20230406/2023040622447.jpg",
+                                    height: 140,
+                                    width: 195,
+                                    fit: BoxFit.cover),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(5),
+                                child: Text("Medway",
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w600,
+                                        color:
+                                            Color.fromRGBO(53, 52, 77, 1.0))),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    "https://seniorassistant.oss-cn-hangzhou.aliyuncs.com/zust-lcy-path/20230406/2023040622447.jpg",
+                                    height: 140,
+                                    width: 195,
+                                    fit: BoxFit.cover),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(5),
+                                child: Text("Medway",
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w600,
+                                        color:
+                                        Color.fromRGBO(53, 52, 77, 1.0))),
+                              ),
+                            ],
+                          ),
+
+                        ],
+                      ),
+                    ),
                     GridView.count(
+                      shrinkWrap: true,
                       crossAxisSpacing: 10.0,
                       //水平子 Widget 之间间距
                       mainAxisSpacing: 10.0,
@@ -193,6 +274,23 @@ class _GroupMainState extends State<GroupMain> with TickerProviderStateMixin {
                       //宽度和高度的比例
                       children: _getListData(),
                     )
+                    /*Container(
+                        height: 100,
+                        width: double.infinity,
+                        child:  GridView.count(
+                          shrinkWrap: true,
+                          crossAxisSpacing: 10.0,
+                          //水平子 Widget 之间间距
+                          mainAxisSpacing: 10.0,
+                          //垂直子 Widget 之间间距
+                          padding: const EdgeInsets.all(10),
+                          crossAxisCount: 2,
+                          //一行的 Widget 数量
+                          childAspectRatio: 0.7,
+                          //宽度和高度的比例
+                          children: _getListData(),
+                        )
+                    )*/
                   ],
                 ),
               )
