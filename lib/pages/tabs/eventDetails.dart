65060b8e695ddab891633796c692938546e454b2
JimStevens-YZY <1527296253@qq.com>
2024-04-21 12:13:32 +0100

    Remove Location dependencies

--- lib/pages/tabs/eventDetails.dart
+++ lib/pages/tabs/eventDetails.dart
@@ -12,9 +12,6 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart' as ph;
